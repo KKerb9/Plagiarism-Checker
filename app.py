2461cf72dba1babbd468aa9cc26affe4caf1aad5
KKerb9 <lukinkd.prog@gmail.com>
2024-04-15 17:01:03 +0300

    Оно красивое но не работает(

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
