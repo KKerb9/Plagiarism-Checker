0b4961cabba6b8cb0526b4775c8275e19f9e8718
KKerb9 <lukinkd.prog@gmail.com>
2024-04-17 00:55:45 +0300

    i wanna sleep

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/get', methods=['POST'])
+def get():
+    data = request.json
+    # распаковка JSON
+    # дальнейшие действия (бекенд)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
